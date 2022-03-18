56c5d7b56fd05094894cf73bc5d9e20fce38c841
ssdsehar <sehar@halimsol.com>
2022-03-18 14:43:24 +0500

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
+import { ChangeContext, LabelType, Options } from '@angular-slider/ngx-slider';
+import { DatePipe } from '@angular/common';
+import { decimalDigest } from '@angular/compiler/src/i18n/digest';
 import { Component } from '@angular/core';
-
+import { LimitDataModel, SliderTickEventArgs, SliderTooltipEventArgs, SliderType, TicksDataModel, TooltipDataModel } from '@syncfusion/ej2-angular-inputs';
+import * as moment from 'moment';
+export interface days {
+  days: string;
+  
+}
+export interface hours {
+  hours: string;
+  
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'schedules';
+  selectedItem = null;
+  documents: any[] = [];
+  selected: any[] = [];
+  endTime:any
+  durationInMinutes:any
+  startTime = '00:00';
+  time: any;
+  date: any;
+  klo:any
+  dateString:any
+  newvalue:any
+ 
+  ngOnInit() {
+   
+    //this.minValue = moment(this.minValue, 'HH:mm').format('HH:mm');
+   this.dateString = moment.unix(this.minValue).format("HH:mm");
+   // var day = moment(this.minValue);
+    console.log(this.dateString)
+  }
+
+  start: any = 43200000 ; // start time in milli Seconds
+  end: any = 86400000;  // end time in milli Seconds
+  
+  minValue = this.start
+  maxValue: number = this.end;
+
+  options: Options = {
+    floor: this.start,
+    ceil: this.end,
+   
+    translate: (value: number, label: LabelType): string => {
+      return new Date(value).toTimeString(); // this will translate label to time stamp.
+    
+    }
+
+    
+  };
+
+ 
 }
+
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TimeComponent } from './time/time.component';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { DragToSelectModule } from 'ngx-drag-to-select';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { SliderModule } from '@syncfusion/ej2-angular-inputs';
+import { Ng5SliderModule } from 'ng5-slider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent
   ],
   imports: [
+    Ng5SliderModule,
+    SliderModule,
     BrowserModule,
-    AppRoutingModule
+    NgxSliderModule,
+    AppRoutingModule,
+    FormsModule,
+    DragToSelectModule.forRoot()
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
